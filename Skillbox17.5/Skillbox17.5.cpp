a97c06352c9bef1fa6df8c3be798ddb252f33e42
Timur Azim <papaas73@gmail.com>
2024-03-05 18:55:50 +0500

    DZ

---
+++ Skillbox17.5/Skillbox17.5.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+
+class Vector
+{
+public:
+	Vector()
+	{}
+	Vector(double _x, double _y, double _z) : x(_x), y(_y), z(_z)
+	{}
+	void Show()
+	{
+		std::cout << '\n' << x << ' ' << y << ' ' << z;
+	}
+	double length() const {
+		return std::sqrt(x * x + y * y + z * z);
+	}
+private:
+	double x = 1;
+	double y = 2;
+	double z = 4;
+};
+
+int main()
+{
+	Vector v; 
+	v.Show();
+	std::cout << "\n Length of the vector: " << v.length() << std::endl;
+}
